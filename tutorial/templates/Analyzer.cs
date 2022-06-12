c1342d3a2bc35d3909e1bd0bdf1667d9c3b9b1fb
obsol <33932119+read-0nly@users.noreply.github.com>
2022-06-12 15:59:57 -0400

    Update Analyzer.cs

--- tutorial/templates/Analyzer.cs
+++ tutorial/templates/Analyzer.cs
@@ -1,15 +1,8 @@
-//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-
 using Oxide.Core;
 using Oxide.Core.Plugins;
 using Oxide.Core.Libraries.Covalence;
 using Oxide.Plugins;
 using UnityEngine; 
-
-//using Rust.Ai;
-//using Network;
 
 namespace Oxide.Plugins
 {
