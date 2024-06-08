b3651a884a553768dea269e81d7ec32d4d59288d
obsol <33932119+read-0nly@users.noreply.github.com>
2024-06-08 12:02:19 -0400

    Add files via upload

--- Oxide/plugins/Navmesher.cs
+++ Oxide/plugins/Navmesher.cs
@@ -33,120 +33,335 @@ namespace Oxide.Plugins
 		private void SendChatMsg(BasePlayer pl, string msg) =>
 			_rustPlayer.Message(pl, msg,  "<color=#00ff00>[Analyzer]</color>", 0, Array.Empty<object>());
 			
+		private Type navtype;
+		void Loaded(){
+			//ConVar.AI.npc_enable=false;
+			//NextFrame(scan3);
+			System.Reflection.Assembly assembly = System.Reflection.Assembly.LoadFrom(".\\RustDedicated_Data\\Managed\\Unity.AI.Navigation.dll");
+			AppDomain.CurrentDomain.Load(assembly.GetName());
+			navtype = assembly.GetType("Unity.AI.Navigation.NavMeshSurface");
+			Puts(navtype.ToString());
+		}
+		void OnTerrainInitialized(){
 			
-		
+			foreach (MonumentInfo monumentInfo in TerrainMeta.Path.Monuments)
+			{
+				monumentInfo.HasNavmesh=false;
+            }
+			scanHurtTrig();
+            NextFrame(scan1);
+			//NextFrame(scan2);
+			NextFrame(scan3);
+			NextFrame(scan4);
+			GameObject ocean = WaterSystem.Ocean.Transform.gameObject;
+            NavMeshObstacle nmo = ocean.GetComponent<NavMeshObstacle>();
+            nmo = (nmo == null ? ocean.AddComponent<NavMeshObstacle>() : nmo);
+            nmo.shape = NavMeshObstacleShape.Box;
+            nmo.center = new Vector3(0, 0, 0);
+            nmo.size = new Vector3(4000,0.2f,4000);
+            nmo.carving = true;
+            nmo.enabled = true;
+        }
 		void scan1(){
 			
+			Puts("-----------------SCAN 1-----------------");
 			UnityEngine.Object[] GameobjectList = Resources.FindObjectsOfTypeAll(typeof(MonumentNavMesh));
+			MonumentNavMesh.use_baked_terrain_mesh= false;
 			foreach(MonumentNavMesh go in GameobjectList){
-				Puts("["+(go.transform.parent==null?go.transform.ToString():go.transform.ToString())+" : "+go.NavMeshAgentTypeIndex.ToString()+" : "+ NavMesh.GetSettingsByIndex(go.NavMeshAgentTypeIndex).agentTypeID.ToString()+"]");
-				go.NavMeshAgentTypeIndex=0;
-				go.gameObject.SetActive(false);
-				go.gameObject.SetActive(true);
+				/*
+				assets/bundled/prefabs/autospawn/monument/medium/compound.prefab (UnityEngine.Transform)
+[Navmesher] Compound (UnityEngine.Transform)
+[Navmesher] assets/bundled/prefabs/autospawn/monument/medium/bandit_town.prefab (UnityEngine.Transform)
+[Navmesher] Bandit_town (UnityEngine.Transform)
+[Navmesher] assets/bundled/prefabs/autospawn/monument/arctic_bases/arctic_research_base_a.prefab (UnityEngine.Transform)
+[Navmesher] arctic_research_base_a (UnityEngine.Transform)
+[Navmesher] assets/bundled/prefabs/autospawn/monument/military_bases/desert_military_base_d.prefab (UnityEngine.Transform)
+[Navmesher] desert_military_base_d (UnityEngine.Transform)
+[Navmesher] AIArena (UnityEngine.Transform)
+[Navmesher] XmasGingerbreadDungeon (UnityEngine.Transform)
+[Navmesher] MineDungeon (UnityEngine.Transform)
+[Navmesher] Bunker_Corridor_A (UnityEngine.Transform)
+[Navmesher] Bunker (UnityEngine.Transform)
+[Navmesher] military_tunnel_hapis (UnityEngine.Transform)
+[Navmesher] Bunker_Corridor_A (UnityEngine.Transform)
+[Navmesher] military_tunnel_hapis (UnityEngine.Transform)
+[Navmesher] MineDungeon (UnityEngine.Transform)
+[Navmesher] Bunker (UnityEngine.Transform)
+[Navmesher] AIArena (UnityEngine.Transform)
+[Navmesher] XmasGingerbreadDungeon (UnityEngine.Transform)
+[Navmesher] desert_military_base_d (UnityEngine.Transform)
+[Navmesher] arctic_research_base_a (UnityEngine.Transform)
+[Navmesher] Compound (UnityEngine.Transform)
+[Navmesher] Bandit_town (UnityEngine.Transform)
+
+				*/
+				if( 
+					!go.transform.ToString().ToLower().Contains("dungeon") &&
+					!go.transform.ToString().ToLower().Contains("aiarena") 
+				){
+					if ( 
+						!go.transform.ToString().ToLower().Contains("oilrig") &&
+						!go.transform.ToString().ToLower().Contains("underwater")
+					)
+					{
+						Puts(go.transform.ToString());
+						go.NavMeshAgentTypeIndex = 0;
+						go.agentTypeId = 0;
+						UnityEngine.Object.Destroy(go);
+					}
+					else
+					{
+						go.NavMeshAgentTypeIndex = 0;
+						go.agentTypeId = 0;
+						go.gameObject.SetActive(false);
+						go.gameObject.SetActive(true);
+						go.HasBuildOperationStarted=false;
+						go.UpdateNavMeshAndWait();
+					
+					}
+				}
 			}
+			
+		}
+		void scanHurtTrig(){
+			
+			Puts("-----------------SCAN Hurt-----------------");
+			UnityEngine.Object[] GameobjectList = Resources.FindObjectsOfTypeAll(typeof(TriggerHurtEx));
+			foreach(TriggerHurtEx go in GameobjectList)
+            {
+				if (go.transform == null) continue;
+				Transform parent = go.transform.parent;
+                if (parent == null || !(
+					parent.transform.ToString().Contains("barbed") ||
+					parent.transform.ToString().Contains("barbed")
+					)) continue;
+                Collider col = parent.GetComponentInChildren<Collider>();
+
+                Bounds b = new Bounds(new Vector3(0, 0, 0), new Vector3(10, 5, 10));
+                if (col != null) b = col.bounds;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Adding Barbed NMO for " + parent.transform.name.Split("/\\".ToCharArray()).Last());
+                Console.ResetColor();
+                float min = (b.extents.x > b.extents.z ? b.extents.x : b.extents.z);
+				try
+				{
+					NavMeshObstacle nmo = parent.gameObject.GetComponent<NavMeshObstacle>();
+					nmo=(nmo==null?parent.gameObject.AddComponent<NavMeshObstacle>():nmo);
+					nmo.shape = NavMeshObstacleShape.Box;
+					nmo.center = new Vector3(0, 0, 0);
+					nmo.size = (parent.transform.name.ToString().Contains("corridor_train_tunnel_entrance") ? new Vector3(15, 10, 15) : new Vector3(10, 5, 10));
+					nmo.carving = false;
+					nmo.enabled = true;
+                    Console.Write(" - ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Success");
+                    Console.ResetColor();
+                }
+				catch(Exception ex)
+                {
+                    Console.Write(" - ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Failure");
+                    Console.ResetColor();
+                }
+
+            }
 			
 		}
 		void scan(GameObject go){
 			MonumentNavMesh mnm = go.GetComponentInChildren<MonumentNavMesh>();
 			DungeonNavmesh dun = go.GetComponentInChildren<DungeonNavmesh>();
 			DynamicNavMesh dyn = go.GetComponentInChildren<DynamicNavMesh>();
-			NavMeshSurface nms = go.GetComponentInChildren<NavMeshSurface>();
+			var nms = go.GetComponentInChildren(navtype);
+			ResourceDispenser dispenser = go.GetComponentInChildren<ResourceDispenser>();
 			if(mnm!=null){
 				mnm.NavMeshAgentTypeIndex=0;
-				Puts("MonumentNavmesh updated");
+				mnm.gameObject.SetActive(false);
+				mnm.gameObject.SetActive(true);
+				mnm.HasBuildOperationStarted=false;
+				mnm.UpdateNavMeshAndWait();
 			}
 			if(dun!=null){
 				dun.NavMeshAgentTypeIndex=0;
-				Puts("DungeonNavmesh updated");
+				dun.gameObject.SetActive(false);
+				dun.gameObject.SetActive(true);
+				dun.HasBuildOperationStarted=false;
+				dun.UpdateNavMeshAndWait();
 			}
 			if(dyn!=null){
 				dyn.NavMeshAgentTypeIndex=0;
-				Puts("DynamicNavMesh updated");
+				dyn.gameObject.SetActive(false);
+				dyn.gameObject.SetActive(true);
+				dyn.HasBuildOperationStarted=false;
+				dyn.UpdateNavMeshAndWait();
 			}
+			
 			if(nms!=null){
-				nms.agentTypeID=0;
-				nms.BuildNavMesh();
-				Puts("NavMeshSurface updated");
+				System.Reflection.PropertyInfo pi = navtype.GetProperty("agentTypeID");
+				pi.SetValue(nms,0);
+				pi = navtype.GetProperty("size");
+				pi.SetValue(nms,10000);
+				System.Reflection.MethodInfo mi = navtype.GetMethod("BuildNavMesh");
+				mi.Invoke(nms,null);
 			}
-			go.SetActive(false);
-			go.SetActive(true);
+			if(dispenser!=null && dispenser.transform.name.Contains("cactus")){
+				//Puts("CactusNavmesh");
+				NavMeshObstacle nmo = dispenser.gameObject.GetComponent<NavMeshObstacle>();
+				nmo=(nmo==null?dispenser.gameObject.AddComponent<NavMeshObstacle>():nmo);
+				nmo.shape = NavMeshObstacleShape.Box;
+				nmo.center = new Vector3(0, 0, 0);
+				nmo.size = (new Vector3(10,10,10));
+				nmo.carving = false;
+				nmo.enabled = true;
+			}
 		}
 		
 		void scan2(){
+			Puts("-----------------SCAN 2-----------------");
 			UnityEngine.Object[] GameobjectList = Resources.FindObjectsOfTypeAll(typeof(DungeonNavmesh));
 			foreach(DungeonNavmesh go in GameobjectList){
-				Puts("["+(go.transform.parent==null?go.transform.ToString():go.transform.ToString())+" : "+go.NavMeshAgentTypeIndex.ToString()+" : "+ NavMesh.GetSettingsByIndex(go.NavMeshAgentTypeIndex).agentTypeID.ToString()+"]");
 				go.NavMeshAgentTypeIndex=0;
+				go.agentTypeId=0;
 				go.gameObject.SetActive(false);
 				go.gameObject.SetActive(true);
+				go.HasBuildOperationStarted=false;
+				go.UpdateNavMeshAndWait();
 			}
 			
 		}
 		void scan3(){
+			Puts("-----------------SCAN 3-----------------");
 			UnityEngine.Object[] GameobjectList = Resources.FindObjectsOfTypeAll(typeof(DynamicNavMesh));
 			foreach(DynamicNavMesh go in GameobjectList){
-				Puts("["+(go.transform.parent==null?go.transform.ToString():go.transform.ToString())+" : "+go.NavMeshAgentTypeIndex.ToString()+" : "+ NavMesh.GetSettingsByIndex(go.NavMeshAgentTypeIndex).agentTypeID.ToString()+"]");
 				go.NavMeshAgentTypeIndex=0;
+				go.agentTypeId=0;
 				go.gameObject.SetActive(false);
-				go.gameObject.SetActive(true);
+                go.HasBuildOperationStarted = false;
+                Puts(go.Bounds.ToString() + " : " + go.NavMeshCollectGeometry.ToString());
+                go.gameObject.SetActive(true);
+				//go.NavMeshCollectGeometry = NavMeshCollectGeometry.RenderMeshes;
+                go.UpdateNavMeshAndWait();
 			}
 			
 		}
 		void scan4(){
+			Puts("-----------------SCAN 4-----------------");
+			/*
+			UnityEngine.Object[] GameobjectList= Resources.FindObjectsOfTypeAll(navtype);
+			UnityEngine.Object last = null;
+			System.Reflection.PropertyInfo pi = navtype.GetProperty("size");
+			Puts("Size:"+pi.GetValue(GameobjectList[0]));
+			//pi.SetValue(GameobjectList[0],new Vector3(6000,300,6000));
 			
-			UnityEngine.Object[] GameobjectList= Resources.FindObjectsOfTypeAll(typeof(NavMeshSurface));
-			NavMeshSurface last;
-			(GameobjectList[0] as NavMeshSurface).size+=(GameobjectList[0] as NavMeshSurface).size;
-			foreach(NavMeshSurface go in GameobjectList){
-				Puts("["+(go.transform.parent==null?go.transform.ToString():go.transform.parent.ToString())+" : "+go.agentTypeID.ToString()+" ]");
-				last=go;
-				go.agentTypeID=0;
-				go.BuildNavMesh();
+			foreach(UnityEngine.Object go in GameobjectList){
+				Puts("Size:"+pi.GetValue(go));
+				last=go; 
+				pi = navtype.GetProperty("agentTypeID");
+				pi.SetValue(go,0);
+				
+				System.Reflection.MethodInfo mi = navtype.GetMethod("BuildNavMesh");
+				mi.Invoke(go,null);
 			}
 			
+			*/
 		}
 		void scanAll(){
-			scan1();
-			scan2();
-			scan3();
-			scan4();
+			NextFrame(scan4);
 		}
 		void OnEntitySpawned(BaseNpc bn){
+			int oldID=0;
+			if(bn.GetComponent<BaseNavigator>() ==null || bn.transform.name.ToLower().Contains("cargo")) return;
 			if(bn.GetNavAgent!=null)
-			bn.GetNavAgent.agentTypeID=0;
+				bn.GetNavAgent.agentTypeID=0;
+			BaseNavigator bnav=bn.GetComponent<BaseNavigator>();
+			oldID = bnav.navMeshQueryFilter.agentTypeID;
+			bnav.SetNavMeshEnabled(true);
+			bnav.defaultAreaMask = UnityEngine.AI.NavMesh.AllAreas;
+			bnav.navMeshQueryFilter.areaMask= UnityEngine.AI.NavMesh.AllAreas;
+			bnav.navMeshQueryFilter.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
+			bnav.Agent.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
+			bnav.defaultAreaMask = 25;
+			bnav.navMeshQueryFilter.areaMask= 25;
+            bnav.Resume();
+			
+
+			Vector3 position = 	bn.transform.position;		
+			if(!bnav.Warp(position)){						
+				bnav.Agent.agentTypeID=oldID;
+			}
+			if(!bnav.Warp(position)){	
+				Puts(((char)27)+"[94m"+"Killing "+bn.transform.name);//
+				bn.Kill();
+			}
 		}
-		void OnTerrainInitialized()
+		object OnSaveLoad(Dictionary<BaseEntity, ProtoBuf.Entity> entities)
 		{
-			scanAll();
-		}
-		void OnWorldPrefabSpawned(GameObject gameObject, string category)
-		{
-			scan(gameObject);
+			//scanAll();
+			return null;
 		}
 		void OnEntitySpawned(CargoShip bn){
+			if(bn==null && bn.gameObject==null) return;
 			scan(bn.gameObject);
 		}
-		void OnEntitySpawned(HumanNPC bn){
-			if(!bn.transform.name.ToLower().Contains("cargo") ){
-				int oldID = bn.GetComponent<BaseNavigator>().Agent.agentTypeID;
-				bn.GetComponent<BaseNavigator>().defaultAreaMask = UnityEngine.AI.NavMesh.AllAreas;
-				bn.GetComponent<BaseNavigator>().navMeshQueryFilter.areaMask= UnityEngine.AI.NavMesh.AllAreas;
-				bn.GetComponent<BaseNavigator>().navMeshQueryFilter.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
-				bn.GetComponent<BaseNavigator>().Agent.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
+		void OnEntitySpawned(JunkPile bn){
+			if(bn==null && bn.gameObject==null) return;
+			scan(bn.gameObject);
+		}
+		void OnEntitySpawned(ResourceDispenser bn){
+			if(bn==null && bn.gameObject==null) return;
+			if(bn==null || !bn.transform.name.Contains("cactus")) return;
+			//Puts("CactusNavmesh");
+			NavMeshObstacle nmo = bn.gameObject.GetComponent<NavMeshObstacle>();
+			nmo=(nmo==null?bn.gameObject.AddComponent<NavMeshObstacle>():nmo);
+			nmo.shape = NavMeshObstacleShape.Box;
+			nmo.center = new Vector3(0, 0, 0);
+			nmo.size = (new Vector3(3,3,3));
+			nmo.carving = false;
+			nmo.enabled = true;
+		}
+		void OnEntitySpawned(NPCPlayer bn){
+				BaseNavigator bnav = bn.GetComponent<BaseNavigator>();
+				if(bnav==null)return;
+				int oldID = bnav.Agent.agentTypeID;
+				bnav.defaultAreaMask = 25;
+				bnav.navMeshQueryFilter.areaMask= 25;
+				bnav.navMeshQueryFilter.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
+				bnav.Agent.agentTypeID= NavMesh.GetSettingsByIndex(0).agentTypeID;
 				
-				Vector3 position = 	bn.transform.position;		
-			
-				if(!bn.GetComponent<BaseNavigator>().Warp(position)){						
-					bn.GetComponent<BaseNavigator>().Agent.agentTypeID=oldID;
+				Vector3 position = 	bn.transform.position;	
+				if(!bnav.PlaceOnNavMesh(0)){						
+					bnav.Agent.agentTypeID=oldID;
 				}
-				if(!bn.GetComponent<BaseNavigator>().Warp(position)){	
-					Puts(((char)27)+"[94m"+"Killing "+bn.transform.name);//
-					bn.Kill();
+				if(!bnav.PlaceOnNavMesh(0))
+            {	
 				}
-			}
+				
 		}
 		private void OnServerInitialized()
         {
+			ConVar.AI.npc_enable=true;
+			UnityEngine.Object[] bandittrigger= Resources.FindObjectsOfTypeAll(typeof(TriggerBanditZone));
+			foreach(TriggerBanditZone hn in bandittrigger){
+				try{GameObject.Destroy(hn);}catch(Exception e){}
+				
+			}
+			bandittrigger= Resources.FindObjectsOfTypeAll(typeof(ResourceDispenser));
+			foreach(ResourceDispenser rd in bandittrigger){
+				OnEntitySpawned(rd);
+				
+			}
+            //NextFrame(flushAll);
+        }
+		void flushAll(){
+			UnityEngine.Object[] humans= Resources.FindObjectsOfTypeAll(typeof(NPCPlayer));
+			UnityEngine.Object[] animals= Resources.FindObjectsOfTypeAll(typeof(BaseNpc));
+			foreach(NPCPlayer hn in humans){
+				try{hn.Kill();}catch(Exception e){}
+				
+			}
+			foreach(BaseNpc bn in animals){
+				try{bn.Kill();}catch(Exception e){}
+			}
 		}
 		public Transform getLookingAtRaw(BasePlayer player){			
 			RaycastHit hit;
